a23e400d411ad6d228b76a784a81d94b079acde8
Monika Kuczkowska <monkuc@st.amu.edu.pl>
2021-05-24 14:52:39 +0200

    initialize the project

---
+++ src/main/java/com/wmi/spizarnia_domowa/SpizarniaDomowaApplication.java
@@ -0,0 +1,13 @@
+package com.wmi.spizarnia_domowa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpizarniaDomowaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpizarniaDomowaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wmi/spizarnia_domowa/SpizarniaDomowaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wmi.spizarnia_domowa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpizarniaDomowaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
